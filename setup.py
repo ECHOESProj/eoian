545503f64bc42639e7511e4152f2c027318bf07f
johnlavelle <git@lavelle.eu>
2022-05-05 19:22:06 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+
+inst_reqs = read_requirements("requirements.txt")
+
+setup(
+    name='eoian',
+    version='0.1',
+    packages=find_packages(exclude=["tests"]),
+    inst_reqs=inst_reqs,
+    url='',
+    license='',
+    author='John Lavelle',
+    author_email='jlavelle@compass.ie',
+    description='EO processing chains'
+)
